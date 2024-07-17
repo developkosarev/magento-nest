e734216f102e88c91bb1e6d6c75b493e9d18c665
developkosarev <develop.kosarev@gmail.com>
2024-07-17 18:43:38 +0200

    Add rabbit

--- apps/cron/src/app/app.module.ts
+++ apps/cron/src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import * as Joi from 'joi';
+import { RmqModule } from '@app/common-nest';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { ScheduleModule } from '@nestjs/schedule';
 import { CronjobsModule } from './cronjobs/cronjobs.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), CronjobsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_BILLING_QUEUE: Joi.string().required(),
+      }),
+    }),
+
+    ScheduleModule.forRoot(),
+    CronjobsModule,
+    RmqModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/common-nest/src/index.ts
+++ libs/common-nest/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/common-nest.module';
+
+export * from './rmq/rmq.service';
+export * from './rmq/rmq.module';
