bc3df2c0496b9151d71b08ff1f480e154e0129a9
developkosarev <develop.kosarev@gmail.com>
2024-07-20 14:04:32 +0200

    Add newsletter

--- apps/newsletter/src/cron/cron.controller.ts
+++ apps/newsletter/src/cron/cron.controller.ts
@@ -1,8 +1,13 @@
 import {Controller, Get} from '@nestjs/common';
 import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 
+import { RmqService } from '@app/common-nest';
+
 @Controller()
 export class CronController {
+  constructor(
+    private readonly rmqService: RmqService
+  ) {}
 
   @Get()
   async getData() {
@@ -12,5 +17,6 @@ export class CronController {
   @EventPattern('reminder_email_task')
   async notifyEmail(@Payload() data: any, @Ctx() context: RmqContext) {
     console.log(1111)
+    this.rmqService.ack(context);
   }
 }
--- apps/newsletter/src/main.ts
+++ apps/newsletter/src/main.ts
@@ -1,11 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { RmqService } from '@app/common-nest';
 
 //import { AppModule } from './app/app.module';
 import { CronModule } from './cron/cron.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CronModule);
+
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('CRON'));
 
   await app.startAllMicroservices();
 
